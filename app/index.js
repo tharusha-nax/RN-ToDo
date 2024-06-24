c76a342327448369e2d6ecae1f4b7f86272adad3
tharusha-nax <tharusha.nax@gmail.com>
2024-06-24 22:48:32 +0530

    add a new todo

--- app/index.js
+++ app/index.js
@@ -1,35 +1,56 @@
+import { useState } from "react";
 import { Text, View, Button, Alert, StyleSheet, TextInput, FlatList, SafeAreaView } from "react-native";
 
 const DATA = [
   {
     id: '1',
-    title: 'Meditation'
+    title: 'Meditation',
+    completed: false
   },
   {
     id: '2',
-    title: 'Coding'
+    title: 'Coding',
+    completed: false
   },
   {
     id: '3',
-    title: 'Journaling'
+    title: 'Journaling',
+    completed: false
   },
 ]
 
-const TodoItem = (props) => (
-  <View style={styles.item}>
-    <Text style={styles.inputItem}>{props.item.title}</Text>
-  </View>
-)
+const TodoItem = (props) => {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.itemText}>{props.item.title}</Text>
+    </View>
+  )
+}
 
 export default function Index() {
+  const [items, setItems] = useState(DATA);
+  const [text, setText] = useState('');
+
+  const addNewTodo = () => {
+    let newTodo = {
+      id: items.length + 1,
+      title: text,
+      completed: false
+    }
+
+    setItems([...items, newTodo]);
+    setText('');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TextInput style={styles.input} />
-      <Button title="Start" onPress={() => Alert.alert('Simple Button Pressed')} />
+      <TextInput style={styles.input} onChangeText={setText} value={text} />
+      <Button title="Add ToDo" onPress={addNewTodo} />
       <FlatList
-        data={DATA}
-        renderItem={({ item }) =>  <TodoItem item = { item } />}
+        style={styles.list}
+        data={items}
+        renderItem={({ item }) => <TodoItem item={item} />}
         keyExtractor={item => item.id} />
     </SafeAreaView>
   );
@@ -48,13 +69,18 @@ const styles = StyleSheet.create({
     padding: 10,
     borderColor: 'gray',
   },
+  list: {
+    alignSelf: 'stretch'
+  },
   item: {
     backgroundColor: '#6DB6DD',
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16
+    marginHorizontal: 16,
+    borderRadius: 10,
   },
-  inputItem: {
-    color: 'black'
+
+  itemText: {
+    color: 'white',
   }
 })
